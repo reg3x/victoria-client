b7b6f3eb31d3c215907826ecdaa8ed92a75135c6
reg3x <diegoaguilera85@gmail.com>
2015-02-11 12:34:23 -0430

    Implemented Bootstrap modal

--- components/catalog/catalog-ctrl.js
+++ components/catalog/catalog-ctrl.js
@@ -2,24 +2,30 @@ var vicApp = angular.module('CatalogCtrl', [ ]);
 
 
 vicApp.controller('CatalogCtrl', ['$http', '$scope','$routeParams', 'pantyService','brasierService','leggingService',
-                      'creamService','butterService','fraganceService', 'allService', 'localStorageService', '$location',
-                      function ($http, $scope, $routeParams, pantyService,brasierService, leggingService,
-                                 creamService, butterService, fraganceService, allService, localStorageService,
-                                  $location) {
+                                  'creamService','butterService','fraganceService', 'allService', 'localStorageService',
+                                  '$location',
+                                  function ($http, $scope, $routeParams, pantyService,brasierService, leggingService,
+                                             creamService, butterService, fraganceService, allService, localStorageService,
+                                             $location) {
 
                           console.dir('parameter provided: '+ $routeParams.section);
                           var options = ['panty', 'brasier','legging', 'cream','butter', 'fragance', 'all'];
                           
-                        
                           $scope.addToCart = function (item) {
                               console.log('item: '+item);
                               var queries = localStorageService.get('items') || [];
                               queries.push(item);
                               console.log('queries: '+queries);
                               localStorageService.set('items', queries);
-                              alert('Se ha agregado a su carro el producto '+item.name);
+                              
+                              $('#addToCartModal').on('show.bs.modal', function (event) {
+                                  var modal = $(this);
+                                  modal.find('.modal-title').text('Added to cart ');
+                                  modal.find('.modal-body .content').text('The item: ' + item.name+ ' has been added.');
+                              })
+                              
+                              $('#addToCartModal').modal();
                           };
-
                           
                           if (typeof $routeParams.section === 'undefined') {
                               $location.path('/catalog/all');
